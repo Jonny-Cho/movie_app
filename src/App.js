d6a3dc48c4814ca93f3713e55286d4bb13b754c2
Jonny <andante2183@gmail.com>
2018-03-21 18:24:12 +1100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie';
+
+const movies = [
+	"Matrix",
+	"Full Metal Jacket",
+	"Oldboy",
+	"Star Wars"
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+      </div>
+    );
+  } 
+}
+
+export default App;
